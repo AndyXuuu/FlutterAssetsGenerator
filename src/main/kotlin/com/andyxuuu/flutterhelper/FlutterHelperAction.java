cff90b4e6c5eb2c4507e8776156e76343f38b33f
18612520062 <andy.xuuu@gmail.com>
2024-02-23 18:30:52 +0800

    init plugin

---
+++ src/main/kotlin/com/andyxuuu/flutterhelper/FlutterHelperAction.java
@@ -0,0 +1,13 @@
+package com.andyxuuu.flutterhelper;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class FlutterHelperAction extends AnAction {
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
